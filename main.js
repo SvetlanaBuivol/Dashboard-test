4f6ac7e557d83cc1955a74866305f48055c1c2ad
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-06-21 15:16:43 +0300

    main.js

--- main.js
+++ main.js
@@ -1,53 +1,52 @@
 const showActiveMembersButton = document.getElementById("activeMembers");
-const showAllMembersButton = document.getElementById('allMembers');
-const allMembers = document.getElementById("dataTable").getElementsByTagName("tr");
+const showAllMembersButton = document.getElementById("allMembers");
+// const allMembers = document.getElementById("dataTable").getElementsByTagName("tr");
+const allMembers = document.querySelectorAll("#dataTable tbody tr");
 let allMembersShown = false;
 
 showActiveMembersButton.addEventListener("click", function () {
-    filterMembers(true)
-    showActiveMembersButton.classList.toggle('active-button')
-    showAllMembersButton.classList.toggle('active-button')
+  filterMembers(true);
+  showActiveMembersButton.classList.toggle("active-button");
+  showAllMembersButton.classList.toggle("active-button");
 });
 
-showAllMembersButton.addEventListener('click', function () {
-    filterMembers(false)
-    showActiveMembersButton.classList.toggle('active-button')
-    showAllMembersButton.classList.toggle('active-button')
-})
+showAllMembersButton.addEventListener("click", function () {
+  filterMembers(false);
+  showActiveMembersButton.classList.toggle("active-button");
+  showAllMembersButton.classList.toggle("active-button");
+});
 
 function filterMembers(showActive) {
-  for (let i = 1; i < allMembers.length; i++) {
-    const statusCell = allMembers[i].getElementsByTagName("td")[5];
+  allMembers.forEach((row) => {
+    const statusCell = row.getElementsByTagName("td")[5];
     const status = statusCell
       ? statusCell.textContent.trim().toLowerCase()
       : "";
-
     if (showActive && status !== "active") {
-      allMembers[i].style.display = "none";
+      row.style.display = "none";
     } else {
-      allMembers[i].style.display = "";
+      row.style.display = "";
     }
-  }
+  });
 }
 
 // input search
-const searchInput = document.getElementById('searchInput')
-// const dataTable = document.getElementById('dataTable')
+const searchInput = document.getElementById("searchInput");
 
-searchInput.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase().trim()
+searchInput.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase().trim();
 
-    Array.from(allMembers).forEach(member => {
-        let memberMatchesSearch = false;
-        Array.from(member.getElementsByTagName('td')).forEach(cell => {
-            if (cell.textContent.toLowerCase().includes(searchTerm)) {
-                memberMatchesSearch = true
-            }
-        })
-        if (memberMatchesSearch) {
-            member.style.display = ''
-        } else {
-            member.style.display = 'none'
-        }
-    })
-})
+  Array.from(allMembers).forEach((member) => {
+    let memberMatchesSearch = false;
+    Array.from(member.getElementsByTagName("td")).forEach((cell) => {
+      if (cell.textContent.toLowerCase().includes(searchTerm)) {
+        memberMatchesSearch = true;
+      }
+    });
+    if (memberMatchesSearch) {
+      member.style.display = "";
+    } else {
+      member.style.display = "none";
+    }
+  });
+});
